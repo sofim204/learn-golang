7f9754e02134dab631943263df02c38cd28363ed
muhammadsofi <sofimuhammad25@gmail.com>
2022-07-05 15:34:19 +0700

    call a module (greetings)

--- hello_world/hello.go
+++ hello_world/hello.go
@@ -3,10 +3,14 @@ package main
 import (
 	"fmt"
 
+	"example.com/greetings"
 	"rsc.io/quote" // importing from external package
 )
 
 func main() {
 	fmt.Println(quote.Go())
 	fmt.Println("Halo sop!")
+	// get greeting message and print it.
+	message := greetings.Hello("Muhammad Sofi")
+	fmt.Println(message)
 }
